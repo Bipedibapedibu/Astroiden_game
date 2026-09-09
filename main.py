a45c8bbe8501d488207bdb412f082d98bddb71d4
Bipedibapedibu <fgffg@gmail.com>
2026-09-09 21:20:44 +0200

    fertig mit Player ertser schritt Astroiden

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -8,16 +12,28 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (drawable,updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     Clock = pygame.time.Clock()
     dt: float = 0.0
+    player = Player(x = SCREEN_WIDTH / 2,y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
     while True:
         log_state()
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
+        for objekt in drawable:
+            objekt.draw(screen)
         dt = Clock.tick(60) / 1000
         pygame.display.flip()
+       
 
 if __name__ == "__main__":
     main()
